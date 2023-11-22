c8ed76cc42e9b5d03d1384fcb6299c282287be85
Luis-f4 <luisbittner1.0@gmail.com>
2023-11-22 14:17:08 +0100

    Setbearbeite.html hinzugefügt (nicht komplett fertig)

---
+++ public/Js/Setbearbeiten.js
@@ -0,0 +1,18 @@
+console.log("TEst");
+
+var popup = document.getElementById("Popup");
+
+
+function Popupoeffnen() {
+    
+
+        popup.style.display = "block";
+
+}
+
+function PopupSchliessen() {
+    
+
+    popup.style.display = "none";
+
+}
